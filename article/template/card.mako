1e6c5278cecad40e5daba4a78177958a8010d298
Like Ma <milkpku@gmail.com>
2018-06-17 14:50:59 +0800

    implement card.mako

---
+++ article/template/card.mako
@@ -0,0 +1,19 @@
+<div class="card">
+  <!--Card image-->
+    <img class="card-img" src="${data['card_img']}" alt="Card image cap">
+
+    <!--Card content-->
+    <div class="card-body">
+      <!-- link to article page -->
+      <a class="card-link-cover" href="${data['dir_path']}/article.html"></a>
+
+      <!-- title of card -->
+      <h3 class="card-title"> ${data['title']} </h3>
+
+      <!-- abstract/introduction -->
+      <p> ${data['abstract']} </p>
+
+      <!-- tags -->
+      <span class="badge badge-pill badge-info"> ${data['tag']} </span>
+    </div>
+</div>
--- render.py
+++ render.py
@@ -1,24 +1,70 @@
 from mako.template import Template
 from mako.lookup import TemplateLookup
 import json
+import os
+
+TEMPLATE_DIR = 'article/template/'
 
 """
-    render article by article folder path
+  load json data from info.json
 """
-def render_article(path):
-    # load data
-    data_io = open(path + "/info.json", 'rb')
-    data_content = data_io.read().decode('utf-8')
-    data = json.loads(data_content)
+def load_json(path):
+  data_io = open(path + "/info.json", 'rb')
+  data_content = data_io.read().decode('utf-8')
+  data = json.loads(data_content)
 
-    # load template
-    mylook = TemplateLookup(['.'], input_encoding='utf-8', output_encoding='utf-8');
+  data["content_ref"] = "../../" + path + '/' + data["content_ref"]
+  data["dir_path"] = path
 
-    mytemp = Template(filename='%s/article.mako' % path, lookup=mylook)
+  return data
 
-    # render
-    with open("%s/article.html" % path, 'wb') as wh:
-        wh.write(mytemp.render_unicode(data=data).encode('utf-8'))
+
+"""
+  render article by article folder path
+"""
+def render_article(data):
+  path = data["dir_path"]
+  # load template
+  mylook = TemplateLookup(['.', path], input_encoding='utf-8', output_encoding='utf-8');
+
+  mytemp = Template(filename=TEMPLATE_DIR + "article.mako", lookup=mylook)
+
+  # render
+  with open("%s/article.html" % path, 'wb') as wh:
+    wh.write(mytemp.render_unicode(data=data).encode('utf-8'))
+
+def render_card(data):
+  path = data["dir_path"]
+
+  # load template
+  mylook = TemplateLookup(['.', path], input_encoding='utf-8', output_encoding='utf-8');
+
+  mytemp = Template(filename=TEMPLATE_DIR + "card.mako", lookup=mylook)
+
+  # render
+  with open("%s/card.html" % path, 'wb') as wh:
+    wh.write(mytemp.render_unicode(data=data).encode('utf-8'))
+
+"""
+  final render
+"""
+def render(path):
+  print("generating %s ... " % path)
+
+  data = load_json(path)
+  
+  render_article(data)
+
+  render_card(data)
+
 
 if __name__=="__main__":
-    render_article("article/template")
+  #render("article/template")
+  #render("article/2017-12-16-linear-lib_test")
+  #from IPython import embed; embed()
+
+  BLOG_DIR = "article"
+  for dirname in os.listdir(BLOG_DIR):
+    filename = os.path.join(BLOG_DIR, dirname, "info.json")
+    if (os.path.isfile(filename)):
+      render(os.path.join(BLOG_DIR, dirname))
